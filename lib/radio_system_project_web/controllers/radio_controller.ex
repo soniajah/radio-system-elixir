b36492d1bd3f98fdbe740697d2111f89d1f0b21b
sonia <soniajah@hotmail.com>
2020-08-30 23:11:28 +0200

    changed radio_controller.ex

--- lib/radio_system_project_web/controllers/radio_controller.ex
+++ lib/radio_system_project_web/controllers/radio_controller.ex
@@ -6,40 +6,40 @@ defmodule RadioSystemProjectWeb.RadioController do
 
   action_fallback RadioSystemProjectWeb.FallbackController
 
-  def index(conn, _params) do
-    radios = Radiosystem.list_radios()
-    render(conn, "index.json", radios: radios)
-  end
+  # def index(conn, _params) do
+  #   radios = Radiosystem.list_radios()
+  #   render(conn, "index.json", radios: radios)
+  # end
 
-  def create(conn, %{"radio" => radio_params}) do
-    with {:ok, %Radio{} = radio} <- Radiosystem.create_radio(radio_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.radio_path(conn, :show, radio))
-      |> render("show.json", radio: radio)
-    end
-  end
+  # def create(conn, %{"radio" => radio_params}) do
+  #   with {:ok, %Radio{} = radio} <- Radiosystem.create_radio(radio_params) do
+  #     conn
+  #     |> put_status(:created)
+  #     |> put_resp_header("location", Routes.radio_path(conn, :show, radio))
+  #     |> render("show.json", radio: radio)
+  #   end
+  # end
 
-  def show(conn, %{"id" => id}) do
-    radio = Radiosystem.get_radio!(id)
-    render(conn, "show.json", radio: radio)
-  end
+  # def show(conn, %{"id" => id}) do
+  #   radio = Radiosystem.get_radio!(id)
+  #   render(conn, "show.json", radio: radio)
+  # end
 
-  def update(conn, %{"id" => id, "radio" => radio_params}) do
-    radio = Radiosystem.get_radio!(id)
+  # def update(conn, %{"id" => id, "radio" => radio_params}) do
+  #   radio = Radiosystem.get_radio!(id)
 
-    with {:ok, %Radio{} = radio} <- Radiosystem.update_radio(radio, radio_params) do
-      render(conn, "show.json", radio: radio)
-    end
-  end
+  #   with {:ok, %Radio{} = radio} <- Radiosystem.update_radio(radio, radio_params) do
+  #     render(conn, "show.json", radio: radio)
+  #   end
+  # end
 
-  def delete(conn, %{"id" => id}) do
-    radio = Radiosystem.get_radio!(id)
+  # def delete(conn, %{"id" => id}) do
+  #   radio = Radiosystem.get_radio!(id)
 
-    with {:ok, %Radio{}} <- Radiosystem.delete_radio(radio) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+  #   with {:ok, %Radio{}} <- Radiosystem.delete_radio(radio) do
+  #     send_resp(conn, :no_content, "")
+  #   end
+  # end
 
   def api_create(conn, %{"id"=>id, "radio" => radio_params}) do
     with {:ok, %Radio{} = radio} <- Radiosystem.create_radio_with_id(String.to_integer(id), radio_params) do
